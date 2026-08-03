98e46c849b3e78e4b8beb3423ebb4b2a0e699799
Saoirse Kelleher <saoirse.kelleher@unimelb.edu.au>
2026-08-03 14:56:16 +1000

    First version of shiny app with the baseline predictions

--- H5N1_Translocation_Sim/server.R
+++ H5N1_Translocation_Sim/server.R
@@ -1,28 +1,158 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Server logic for H5N1 simulator
 
+library(tidyverse)
 library(shiny)
+library(cmdstanr)
+
+# Function to make predictions
+plot_baseline <- function(model_data, nreps){
+  all_data <- data.frame()
+  for (i in 1:nreps){
+    n0 <- rnorm(1,
+                mean = model_data$n_0_mu, 
+                sd = model_data$n_0_sd)
+    base_lambdaw <- rnorm(1,
+                          mean = model_data$mu_base_lambdaw,
+                          sd = model_data$sd_base_lambdaw)
+    flu_lambdaw <- rnorm(1,
+                         mean = model_data$mu_flu_lambdaw,
+                         sd = model_data$sd_flu_lambdaw)
+    flu<-vector()
+    for (t in 1:9){
+      flu[t] <- as.numeric(runif(1,0,1) < model_data[["pr_flu"]][t])
+    }
+    row_data <- data.frame(i = i,
+                           t = 1:10, 
+                           n = NA)
+    row_data[row_data$t == 1, "n"] <- n0
+    for (t in 2:10){
+      row_data$n[row_data$t==t] <- row_data$n[row_data$t==t-1]*base_lambdaw*(1-flu[t-1])+
+        row_data$n[row_data$t==t-1]*flu_lambdaw*(flu[t-1])
+      
+      if (row_data$n[row_data$t==t] <= 0){
+        row_data$n[row_data$t==t] <- 0
+      }
+    }
+    
+    all_data <- rbind(all_data, row_data)
+  }
+  
+  all_data |>
+    ggplot(aes(x = t, y = n, group = i)) +
+    geom_line(alpha = 0.1)
+}
+get_baseline_surv <- function(model_data, nreps){
+  all_data <- data.frame()
+  for (i in 1:nreps){
+    n0 <- rnorm(1,
+                mean = model_data$n_0_mu, 
+                sd = model_data$n_0_sd)
+    base_lambdaw <- rnorm(1,
+                          mean = model_data$mu_base_lambdaw,
+                          sd = model_data$sd_base_lambdaw)
+    flu_lambdaw <- rnorm(1,
+                         mean = model_data$mu_flu_lambdaw,
+                         sd = model_data$sd_flu_lambdaw)
+    flu<-vector()
+    for (t in 1:9){
+      flu[t] <- as.numeric(runif(1,0,1) < model_data[["pr_flu"]][t])
+    }
+    row_data <- data.frame(i = i,
+                           t = 1:10, 
+                           n = NA)
+    row_data[row_data$t == 1, "n"] <- n0
+    for (t in 2:10){
+      row_data$n[row_data$t==t] <- row_data$n[row_data$t==t-1]*base_lambdaw*(1-flu[t-1])+
+        row_data$n[row_data$t==t-1]*flu_lambdaw*(flu[t-1])
+      
+      if (row_data$n[row_data$t==t] <= 0){
+        row_data$n[row_data$t==t] <- 0
+      }
+    }
+    
+    all_data <- rbind(all_data, row_data)
+  }
+  
+  outSurv <- all_data |>
+    filter(t == 10) |>
+    mutate(surv = n > 0) |>
+    pull(surv)
+  
+  sum(outSurv)/nreps
+    
+}
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
 
-    output$distPlot <- renderPlot({
+    output$n_w_proj <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+      data.frame(x = (input$n_0_mu-(3*input$n_0_sd)):(input$n_0_mu+(3*input$n_0_sd))) |>
+        mutate(pr = dnorm(x, mean = input$n_0_mu, sd = input$n_0_sd)) |>
+        filter(x > 0) |>
+        ggplot(aes(x = x, y = pr)) +
+        geom_line(colour = "cadetblue4", 
+                  alpha = 0.5, linewidth = 3) +
+        theme_minimal() +
+        theme(axis.text.y = element_blank(), axis.title = element_blank())
 
     })
-
+    
+    output$lambda_proj <- renderPlot({
+      
+      data.frame(x = c(seq((input$base_lambda_mu-(3*input$base_lambda_sd)),
+                           (input$base_lambda_mu+(3*input$base_lambda_sd)),
+                           by = 0.05),
+                       seq((input$flu_lambda_mu-(3*input$flu_lambda_sd)),
+                           (input$flu_lambda_mu+(3*input$flu_lambda_sd)),
+                           by = 0.05)
+                 )) |>
+        mutate(basepr = dnorm(x, mean = input$base_lambda_mu, sd = input$base_lambda_sd),
+               flupr = dnorm(x, mean = input$flu_lambda_mu, sd = input$flu_lambda_sd)) |>
+        pivot_longer(cols = c(basepr, flupr),
+                     names_to = "group", values_to = "pr") |>
+        filter(x > 0) |>
+        ggplot(aes(x = x, y = pr, colour = group)) +
+        geom_line(alpha = 0.5, linewidth = 3) +
+        scale_colour_manual("",
+                            limits = c("basepr", "flupr"),
+                            labels = c("Base", "Flu"),
+                            values = c("cadetblue4", "firebrick")) +
+        theme_minimal() +
+        theme(axis.text.y = element_blank(), axis.title = element_blank())
+      
+    })
+    
+    output$baseline_sims <- renderPlot({
+      model_data <- list(
+        nTimesteps = 10,
+        n_0_mu = input$n_0_mu,
+        n_0_sd = input$n_0_sd,
+        mu_base_lambdaw = input$base_lambda_mu,
+        sd_base_lambdaw = input$base_lambda_sd,
+        mu_flu_lambdaw = input$flu_lambda_mu,
+        sd_flu_lambdaw = input$flu_lambda_mu,
+        pr_flu = c(input$t1_flu, input$t2_flu, input$t3_flu, input$t4_flu,
+                   input$t5_flu, input$t6_flu, input$t7_flu, input$t8_flu,
+                   input$t9_flu)
+      )
+      plot_baseline(model_data, nreps = 1000)
+    })
+    
+    output$baseline_survival <- renderText({
+      model_data <- list(
+        nTimesteps = 10,
+        n_0_mu = input$n_0_mu,
+        n_0_sd = input$n_0_sd,
+        mu_base_lambdaw = input$base_lambda_mu,
+        sd_base_lambdaw = input$base_lambda_sd,
+        mu_flu_lambdaw = input$flu_lambda_mu,
+        sd_flu_lambdaw = input$flu_lambda_mu,
+        pr_flu = c(input$t1_flu, input$t2_flu, input$t3_flu, input$t4_flu,
+                   input$t5_flu, input$t6_flu, input$t7_flu, input$t8_flu,
+                   input$t9_flu)
+      )
+      get_baseline_surv(model_data, nreps = 1000)
+      
+    })
 }
--- H5N1_Translocation_Sim/ui.R
+++ H5N1_Translocation_Sim/ui.R
@@ -1,33 +1,147 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# UI for H5N1 shiny app
 
 library(shiny)
+library(bslib)
 
 # Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+ui <- page_sidebar(
+  title = "H5N1 Simulator",
+  # Create sidebar with input selectors
+  sidebar = sidebar(title = "Set Parameters",
+                    card(
+                      card_header("Initial population"),
+                      sliderInput(
+                        "n_0_mu",
+                        label = "Mean",
+                        min = 0,
+                        max = 10000,
+                        value = 1000
+                      ),
+                      sliderInput(
+                        "n_0_sd",
+                        label = "Std. Deviation",
+                        min = 0,
+                        max = 10000,
+                        value = 100
+                      ),
+                      plotOutput("n_w_proj", height = "100px")
+                    ),
+                    card(
+                      card_header("Baseline growth rate"),
+                      sliderInput(
+                        "base_lambda_mu",
+                        label = "Mean",
+                        min = 0,
+                        max = 3,
+                        value = 1,
+                        step = 0.01
+                      ),
+                      sliderInput(
+                        "base_lambda_sd",
+                        label = "Std. Deviation",
+                        min = 0,
+                        max = 1,
+                        value = .1,
+                        step = 0.01
+                      ),
+                      sliderInput(
+                        "flu_lambda_mu",
+                        label = "Mean",
+                        min = 0,
+                        max = 3,
+                        value = 0.6, 
+                        step = 0.01
+                      ),
+                      sliderInput(
+                        "flu_lambda_sd",
+                        label = "Std. Deviation",
+                        min = 0,
+                        max = 1,
+                        value = .1,
+                        step = 0.01
+                      ),
+                      plotOutput("lambda_proj", height = "100px")
+                    ),
+                    card(
+                      card_header("Probability of flu"),
+                      numericInput("t1_flu",
+                                   "t=1",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                                   ),
+                      numericInput("t2_flu",
+                                   "t=2",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t3_flu",
+                                   "t=3",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t4_flu",
+                                   "t=4",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t5_flu",
+                                   "t=5",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t6_flu",
+                                   "t=6",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t6_flu",
+                                   "t=6",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t6_flu",
+                                   "t=6",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t7_flu",
+                                   "t=7",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t8_flu",
+                                   "t=8",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                      numericInput("t9_flu",
+                                   "t=9",
+                                   value = 0.3,
+                                   min = 0,
+                                   max = 1
+                      ),
+                    )
+                    ),
+  card(
+    card_header("Simulated populations"),
+    card(
+      plotOutput("baseline_sims")
+    ),
+    value_box(
+      title = "Survival rate",
+      value = textOutput("baseline_survival"),
+      theme = "teal"
+    ),
+    
+  ), 
 )
